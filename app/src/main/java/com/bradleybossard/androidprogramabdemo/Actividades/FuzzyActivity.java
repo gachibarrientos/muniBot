0457732c8ca1e9828de936353e23b9ca6c313bad
Gabriel Barrientos <gabrielb_31@hotmail.com>
2016-06-07 13:22:21 -0300

    Agregando la librería FuzzyLogic

--- app/src/main/java/com/bradleybossard/androidprogramabdemo/Actividades/FuzzyActivity.java
+++ app/src/main/java/com/bradleybossard/androidprogramabdemo/Actividades/FuzzyActivity.java
@@ -1,7 +1,44 @@
 package com.bradleybossard.androidprogramabdemo.Actividades;
+import android.content.Context;
 
+
+import net.sourceforge.jFuzzyLogic.*;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+import java.io.InputStream;
 /**
  * Created by Gachi on 7/6/2016.
  */
 public class FuzzyActivity {
+    private Context context;
+    private double nivelTecno;
+    public FuzzyActivity(Context context) {
+        this.context = context;
+    }
+
+    public double FuzzyEngine(Integer edad, Integer educacion) throws Exception {
+        // Load from 'FCL' file
+        InputStream inputStream = context.getAssets().open("chat.fcl");
+        FIS fis = FIS.load(inputStream, true);
+
+        // Error while loading?
+        if (fis == null) {
+            System.err.println("Error al cargar el archivo del controlador difuso '" + inputStream + "'");
+            return nivelTecno;
+        }
+        // Show
+//        JFuzzyChart.get().chart(fis);
+
+        // Set inputs
+        fis.setVariable("Edad", edad);
+        fis.setVariable("Educacion", educacion);
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable nivelT = fis.getVariable("Nivel_Tecnologico");
+        nivelTecno = nivelT.getValue();
+        System.out.println("Nivel Tecnologico es:" + nivelT);
+        return nivelTecno;
+    }
 }
